edfc8189e219883367398c1aaa9333a416f1dacf
Aleksey Zemskov <zemskov.aleksey@gmail.com>
2020-10-18 13:41:12 +0300

    Rename

--- GoogleTranslate/Entities/GoogleTransResult.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace Zaac.GoogleTranslate.Entities
-{
-    internal class GoogleTransResult
-    {
-        internal string TargetText { get; set; }
-
-        internal string From { get; set; }
-    }
-}
--- GoogleTranslate/Enums/TranslationResultTypes.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace Zaac.GoogleTranslate.Enums
-{
-    public enum TranslationResultTypes
-    {
-        Failed = 0,
-        Successed = 1
-    }
-}
